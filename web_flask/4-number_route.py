5fbd090243ffd4cff47a34c8d7597c0ff762da41
rennleon <2829@holbertonschool.com>
2021-09-10 17:11:13 +0000

    Cerate a script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module starts a flask app on 0.0.0.0:5000"""
+
+from flask import Flask, abort
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    """Returns a text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<txt>', strict_slashes=False)
+def c_text(txt):
+    """Returns a text passed as param"""
+    return 'c {}'.format(txt.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<txt>', strict_slashes=False)
+def py_text(txt='is cool'):
+    """Returns a text passed as param"""
+    return 'Python {}'.format(txt.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def num(n):
+    """Returns a text if 'n' is number, gives a 404 otherwise"""
+    if not n.isnumeric():
+        abort(404)
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')

6d370b224502f3203571da02810c9817dd269bac
rennleon <2829@holbertonschool.com>
2021-09-10 17:51:27 +0000

    Create a script that starts a Flask web application and handles some routes

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""This module starts a flask app on 0.0.0.0:5000"""
+
+from flask import Flask, abort, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    """Returns a text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<txt>', strict_slashes=False)
+def c_text(txt):
+    """Returns a text passed as param"""
+    return 'c {}'.format(txt.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<txt>', strict_slashes=False)
+def py_text(txt='is cool'):
+    """Returns a text passed as param"""
+    return 'Python {}'.format(txt.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def num(n):
+    """Returns a text if 'n' is number, gives a 404 otherwise"""
+    if not n.isnumeric():
+        abort(404)
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def num_html(n):
+    """Renders an HTML if 'n' is number, gives a 404 otherwise"""
+    if not n.isnumeric():
+        abort(404)
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def num_html_even_odd(n):
+    """Renders an HTML if 'n' is number, gives a 404 otherwise"""
+    if not n.isnumeric():
+        abort(404)
+
+    even_odd = 'odd'
+    if (int(n) % 2 == 0):
+        even_odd = 'even'
+    return render_template('6-number_odd_or_even.html', n=n, even_odd=even_odd)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')

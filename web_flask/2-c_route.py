62aee08430dd174769559af1a2a3f1c1bbeacbbe
rennleon <2829@holbertonschool.com>
2021-09-11 21:50:04 +0000

    Update return message

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def text(text):
     """Returns a text passed as param"""
-    return 'c {}'.format(text.replace('_', ' '))
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
